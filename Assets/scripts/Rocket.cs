b2ed49b63bf5bbe8fd506d46392917ecc43bbab9
Tanner Gill <contacttannergill@gmail.com>
2019-01-01 14:05:03 -0800

    Wrote basic a basic rocket movement script that does not consider deltaTime.

---
+++ Assets/scripts/Rocket.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour {
+
+    Rigidbody rigidBody;
+
+	// Use this for initialization
+	void Start () {
+        rigidBody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        ProcessInput();
+	}
+
+    private void ProcessInput()
+    {
+        // On space press
+        if (Input.GetKey(KeyCode.Space))
+        {
+            print("adding force");
+            rigidBody.AddRelativeForce(Vector3.up);
+        }
+
+        // The user can only go in one direction at a time.
+        // For that reason, we're using an else if condition to handle rotation movement.
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.forward);
+        } 
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(-Vector3.forward);
+        }
+    }
+}
