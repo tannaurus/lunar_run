f545c81e0e7d0dccb24c670b97325689c26195e1
Tanner Gill <contacttannergill@gmail.com>
2019-01-04 19:18:59 -0800

    Added sound to rocket, a radio, and camera tracking of player

--- Assets/scripts/Rocket.cs
+++ Assets/scripts/Rocket.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour {
 
+    AudioSource engine;
     Rigidbody rigidBody;
 
 	// Use this for initialization
 	void Start () {
         rigidBody = GetComponent<Rigidbody>();
+        engine = GetComponent<AudioSource>();
 	}
 	
 	// Update is called once per frame
@@ -19,22 +21,35 @@ public class Rocket : MonoBehaviour {
 
     private void ProcessInput()
     {
-        // On space press
-        if (Input.GetKey(KeyCode.Space))
-        {
-            print("adding force");
-            rigidBody.AddRelativeForce(Vector3.up);
-        }
+        // On space press, thrust.
+        Thrust();
 
         // The user can only go in one direction at a time.
         // For that reason, we're using an else if condition to handle rotation movement.
         if (Input.GetKey(KeyCode.A))
         {
             transform.Rotate(Vector3.forward);
-        } 
+        }
         else if (Input.GetKey(KeyCode.D))
         {
             transform.Rotate(-Vector3.forward);
+        }
+    }
+
+    // Invoked in the Update method, handles the thrusting motion of the rocket.
+    private void Thrust()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rigidBody.AddRelativeForce(Vector3.up);
+            if (!engine.isPlaying)
+            {
+                engine.Play();
+            }
+        }
+        else
+        {
+            engine.Stop();
         }
     }
 }
